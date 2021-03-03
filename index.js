d49001d667e5b9d77940472ccab3ca5d3a2d5331
thais rocha <thatath938@gmail.com>
2021-03-03 00:49:16 -0300

    conectando com o banco e add .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const customExpress = require('./config/customExpress')
 const app = customExpress()
+const connection = require('./infra//connection')
 
 
-app.listen(3000, ()=> console.log("servidor rodando na porta 3000, acesse: http://localhost:3000"))
+connection.connect( erro => {
+    if(erro){
+        console.log(erro);
+    }
+    else{
+        console.log('Banco de dados conectado com sucesso!!!')
+        app.listen(3000, ()=> console.log("servidor rodando na porta 3000, acesse: http://localhost:3000"))
+    }
+
+})
